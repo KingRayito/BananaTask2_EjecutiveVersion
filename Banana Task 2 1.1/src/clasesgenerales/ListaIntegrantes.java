65ae882cd742c19a425917c3591add8eac224e04
BastiOP <41351019+BastiOP@users.noreply.github.com>
2019-03-25 23:22:14 -0300

    Anidacion de clases

---
+++ Banana Task 2 1.1/src/clasesgenerales/ListaIntegrantes.java
@@ -0,0 +1,41 @@
+package clasesgenerales;
+
+import java.util.HashMap;
+import clasesbasicas.Integrante;
+
+public class ListaIntegrantes {
+	private HashMap<String,Integrante> mapaIntegrantes;
+	
+	public boolean agregarIntegrante(Integrante integrante) {
+		if (mapaIntegrantes.isEmpty())
+			mapaIntegrantes.put(integrante.getIdentificador(), integrante);
+		else {
+			if (mapaIntegrantes.containsKey(integrante.getIdentificador()))
+				return false;
+			else 
+				mapaIntegrantes.put(integrante.getIdentificador(), integrante);
+		}
+		return true;
+	}
+	
+	public boolean removerIntegrante(String identificador) {
+		if (mapaIntegrantes.isEmpty())
+			return false;
+		else
+			mapaIntegrantes.remove(identificador);
+		return true;
+	}
+	
+	public boolean editarIntegrante(Integrante integrante) {
+		if (mapaIntegrantes.isEmpty())
+			return false;
+		else {
+			if (mapaIntegrantes.containsKey(integrante.getIdentificador()))
+				mapaIntegrantes.put(integrante.getIdentificador(), integrante);
+			else 
+				return false;
+		}
+		return true;
+	}
+	
+}
---
+++ Banana Task 2 1.1/src/clasesgenerales/ListaProyectos.java
@@ -0,0 +1,5 @@
+package clasesgenerales;
+
+public class ListaProyectos {
+
+}

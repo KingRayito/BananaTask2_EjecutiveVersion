5333d87fa7e8f2a10c0274ba574dfa0f132a00e6
BastiOP <41351019+BastiOP@users.noreply.github.com>
2019-03-29 00:12:56 -0300

    Banana Task 2 1.1 ACTUALIZADO

--- Banana Task 2 1.1/src/application/BananaTask2.java
+++ Banana Task 2 1.1/src/application/BananaTask2.java
@@ -1,27 +1,125 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import clasesbasicas.Integrante;
+import clasesgenerales.ListaIntegrantes;
 
 
-public class BananaTask2 extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+public class BananaTask2 {
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("Iniciando aplicacion...");
+		BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+		String key, buffer;
+		int op1 = 5;
+		Integrante integrante = new Integrante("admin", "1234", "Nombre", "Admin", true);
+		ListaIntegrantes integrantes = new ListaIntegrantes();
+		integrantes.agregarIntegrante(integrante);
+		do {
+			System.out.println("Bienvenido a BananaTask Enterprise. Por favor inicie sesión para continuar.");
+			do {
+				System.out.println("Ingrese nombre de usuario.");
+				key = lector.readLine();
+				
+				if(integrantes.getMapaIntegrantes().containsKey(key)){
+					
+					System.out.println("Ingrese contraseña del usuario.");
+					do {
+						buffer = lector.readLine();
+						if((integrantes.getMapaIntegrantes().get(key).getContraseña().compareTo(buffer)) == 0) {
+							System.out.println("Inicio de sesion exitoso.");
+							break;
+						}
+						else {
+							System.out.println("Contraseña erronea. Intente nuevamente.");
+						}
+					}while(true);
+					break;
+				}
+				else {
+					System.out.println("Usuario no encontrado. Intente nuevamente");
+				}
+				
+			}while(true);
+			
+			if(integrantes.getMapaIntegrantes().get(key).isAdmin()) {
+				do {
+		
+					System.out.println("1.- Ingresar proyecto.");
+					System.out.println("2.- Mostrar proyectos.");
+					System.out.println("3-. Ingresar usuario");
+					System.out.println("4-. Cerrar sesion");
+					System.out.println("5-. Salir del programa.");
+					op1 = Integer.parseInt(lector.readLine());
+		
+			        switch (op1){
+			        
+			            case 1:
+			            	
+			            	
+			            	
+			            	break;
+			            
+			            case 2: 
+			            	
+			            	
+			            	
+			            	break;
+			            	
+			            case 3:
+			            	
+			            	integrante = new Integrante();
+			            	System.out.println("Ingrese nombre de usuario: ");
+			            	key = lector.readLine();
+			            	integrante.setIdentificador(key);
+			              	System.out.println("Ingrese contraseña de usuario: ");
+			            	buffer = lector.readLine();
+			            	integrante.setContraseña(buffer);
+			              	System.out.println("Ingrese nombre: ");
+			            	buffer = lector.readLine();
+			            	integrante.setNombre(buffer);
+			              	System.out.println("Ingrese oupacion del usuario: ");
+			            	buffer = lector.readLine();
+			            	integrante.setOcupacion(buffer);
+			            	integrante.setAdmin(false);
+			            	
+			            	integrantes.getMapaIntegrantes().put(key, integrante);
+			            	
+			            	break;
+			            	
+			            case 4: 
+			            	System.out.println("Cerrando sesion...");
+			            	break; 
+			            
+			            case 5: break;
+			            
+			            default: break;
 	
-	public static void main(String[] args) {
-		launch(args);
+			        }   
+		
+				} while(op1 < 4);  
+				
+			}
+			else {
+				System.out.println("1-. Cerrar sesion");
+				System.out.println("5-. Salir");
+				op1 = Integer.parseInt(lector.readLine());
+				switch(op1){
+					
+					case 1:
+						System.out.println("Cerrando sesion...");
+						break;
+						
+					case 5: 
+						break;
+					
+					default: 
+						break;
+					
+				}
+			}
+		} while(op1 != 5);
 	}
 }
--- Banana Task 2 1.1/src/clasesgenerales/ListaIntegrantes.java
+++ Banana Task 2 1.1/src/clasesgenerales/ListaIntegrantes.java
@@ -6,6 +6,10 @@ import clasesbasicas.Integrante;
 public class ListaIntegrantes {
 	private HashMap<String,Integrante> mapaIntegrantes;
 	
+	public ListaIntegrantes() {
+		mapaIntegrantes = new HashMap<String,Integrante>();
+	}
+	
 	public boolean agregarIntegrante(Integrante integrante) {
 		if (mapaIntegrantes.isEmpty())
 			mapaIntegrantes.put(integrante.getIdentificador(), integrante);
@@ -37,5 +41,22 @@ public class ListaIntegrantes {
 		}
 		return true;
 	}
+	
+	public boolean inicioSesion(String identificador, String contraseña){
+		for (String k : mapaIntegrantes.keySet()) 
+			if (k.compareTo(identificador) == 0) 
+				if (mapaIntegrantes.get(k).getContraseña().compareTo(contraseña) == 0)
+					return true;
+		return false;
+	}
+
+	public HashMap<String, Integrante> getMapaIntegrantes() {
+		return mapaIntegrantes;
+	}
+
+	public void setMapaIntegrantes(HashMap<String, Integrante> mapaIntegrantes) {
+		this.mapaIntegrantes = mapaIntegrantes;
+	}
+	
 	
 }
